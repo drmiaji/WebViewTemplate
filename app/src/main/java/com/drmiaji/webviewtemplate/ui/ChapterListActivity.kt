c92c0a55e3ca019bfe898f0658cb8ddbf730cfe5
Dr Abdul Baten Miaji <bmiaji@gmail.com>
2025-05-16 16:54:38 +0200

    done

--- app/src/main/java/com/drmiaji/webviewtemplate/ui/ChapterListActivity.kt
+++ app/src/main/java/com/drmiaji/webviewtemplate/ui/ChapterListActivity.kt
@@ -2,8 +2,11 @@ package com.drmiaji.webviewtemplate.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import com.drmiaji.webviewtemplate.R
 import androidx.activity.viewModels
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.drmiaji.webviewtemplate.activity.BaseActivity
@@ -17,6 +20,20 @@ class ChapterListActivity : BaseActivity() {
     override fun getLayoutResource() = R.layout.activity_chapter_list
 
     override fun onActivityReady(savedInstanceState: Bundle?) {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Chapters" // Or dynamic title if you want
+
+        // Set custom font on toolbar title
+        val typeface = ResourcesCompat.getFont(this, R.font.solaimanlipi)
+        for (i in 0 until toolbar.childCount) {
+            val view = toolbar.getChildAt(i)
+            if (view is TextView && view.text == supportActionBar?.title) {
+                view.typeface = typeface
+                break
+            }
+        }
+
         val recyclerView = findViewById<RecyclerView>(R.id.chapter_recycler)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
@@ -24,6 +41,7 @@ class ChapterListActivity : BaseActivity() {
             recyclerView.adapter = ChapterAdapter(list) { chapter ->
                 val intent = Intent(this, WebViewActivity::class.java)
                 intent.putExtra("fileName", chapter.file)
+                intent.putExtra("title", chapter.title)
                 startActivity(intent)
             }
         }
--- app/src/main/java/com/drmiaji/webviewtemplate/ui/WebViewActivity.kt
+++ app/src/main/java/com/drmiaji/webviewtemplate/ui/WebViewActivity.kt
@@ -1,21 +1,41 @@
 package com.drmiaji.webviewtemplate.ui
 
+import android.os.Bundle
+import android.view.MenuItem
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.os.Bundle
-import com.drmiaji.webviewtemplate.activity.BaseActivity
+import androidx.appcompat.widget.Toolbar
 import com.drmiaji.webviewtemplate.R
+import com.drmiaji.webviewtemplate.activity.BaseActivity
 
 class WebViewActivity : BaseActivity() {
 
     override fun getLayoutResource() = R.layout.activity_webview
 
     override fun onActivityReady(savedInstanceState: Bundle?) {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            title = intent.getStringExtra("title") ?: "Reading"
+        }
+
         val webView = findViewById<WebView>(R.id.webview)
         val fileName = intent.getStringExtra("fileName") ?: "chapter1.html"
 
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = false
-        webView.loadUrl("file:///android_asset/contents/chapters/$fileName")
+        webView.loadUrl("file:///android_asset/contents/topics/$fileName")
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
